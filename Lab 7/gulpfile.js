7664daed6157f0c65641575228bb69a75556a8da
Sean Hicks <shicks@stevens.edu>
2022-12-06 18:57:54 -0500

    imported bootstrap

---
+++ Lab 7/gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require("gulp");
+const concatenate = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
+const autoprefixer = require("gulp-autoprefixer");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const gulpSASS = require("gulp-sass")(require("sass"));
